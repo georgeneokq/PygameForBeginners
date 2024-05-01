cbaad0792c981ba9d927be6e37dd4a831dfa4b77
George <george.neokq@gmail.com>
2024-05-01 13:41:29 +0900

    chore: setup and run scripts for dummies

---
+++ setup.cmd
@@ -0,0 +1,20 @@
+REM Operate in Desktop folder
+cd %USERPROFILE%\Desktop
+
+REM Download zip file from github
+curl -Lo pygame.zip "https://github.com/georgeneokq/PygameForBeginners/archive/refs/heads/main.zip"
+
+REM Unzip folder
+tar -xf pygame.zip
+
+REM Rename folder to pygame
+rename PygameForBeginners-main pygame
+
+REM Delete zip file
+del pygame.zip
+
+REM Operate in pygame folder
+cd pygame
+
+REM Install dependencies
+pip install -r requirements.txt
